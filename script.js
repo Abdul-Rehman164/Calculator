ee619c95e410474673e52a93d65c23d2b4d2018f
Abdul Rehman <abdulrehmang571@gmail.com>
2023-03-13 13:26:22 +0500

    Added the operand and operator variable

--- script.js
+++ script.js
@@ -1,32 +1,42 @@
-function add(num1,num2){
-    return num1 + num2;
+function add(num1, num2) {
+  return num1 + num2;
 }
-function subtract(num1,num2){
-    return num1 - num2;
+function subtract(num1, num2) {
+  return num1 - num2;
 }
-function multiply(num1,num2){
-    return num1 * num2;
+function multiply(num1, num2) {
+  return num1 * num2;
 }
-function divide(num1,num2){
-    return num1 / num2;
+function divide(num1, num2) {
+  return num1 / num2;
 }
-function operate(num1,num2,operation){
-    if (operation === '+') return add(num1,num2);
-    else if (operation === '-') return subtract(num1,num2);
-    else if (operation === 'x') return multiply(num1,num2);
-    else if (operation === '/') return divide(num1,num2);
+function operate(num1, num2, operation) {
+  if (operation === "+") return add(num1, num2);
+  else if (operation === "-") return subtract(num1, num2);
+  else if (operation === "x") return multiply(num1, num2);
+  else if (operation === "/") return divide(num1, num2);
 }
+let operand1 = "";
+let operand2 = "";
+let operator = "";
+let result = "";
 
-function getOperand(){
-    const numbers = document.querySelectorAll('.number');
-    numbers.forEach(number => number.addEventListener('click',(e)=>{
-        return e.target.textContent;
-    }));
-}
+const numbers = document.querySelectorAll(".number");
+numbers.forEach((number) =>
+  number.addEventListener("click", (e) => {
+    if (operator) {
+      operand2 += e.target.textContent;
+      console.log({ operand2 });
+    } else {
+      operand1 += e.target.textContent;
+      console.log({ operand1 });
+    }
+  })
+);
 
-function getOperator(){
-    const operators = document.querySelectorAll('.operator');
-    operators.forEach(operator => operator.addEventListener('click',(e)=>{
-        return e.target.textContent;
-    }));
-};
+const operators = document.querySelectorAll(".operator");
+operators.forEach((operatorL) =>
+  operatorL.addEventListener("click", (e) => {
+    operator += e.target.textContent;
+  })
+);
