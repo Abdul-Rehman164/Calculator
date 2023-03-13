e1a1dde5b8bb847c1d0f3405e61d8d22508d2613
Abdul Rehman <abdulrehmang571@gmail.com>
2023-03-13 11:51:46 +0500

    Added the classes to numbers and operators

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 function add(num1,num2){
-    console.log(num1 + num2) ;
+    return num1 + num2;
 }
 function subtract(num1,num2){
-    console.log(num1 - num2) ;
+    return num1 - num2;
 }
 function multiply(num1,num2){
-    console.log(num1 * num2) ;
+    return num1 * num2;
 }
 function divide(num1,num2){
-    console.log(num1 / num2) ;
+    return num1 / num2;
 }
 function operate(num1,num2,operation){
-    if (operation === '+') add(num1,num2);
-    else if (operation === '-') subtract(num1,num2);
-    else if (operation === 'x') multiply(num1,num2);
-    else if (operation === '/') divide(num1,num2);
+    if (operation === '+') return add(num1,num2);
+    else if (operation === '-') return subtract(num1,num2);
+    else if (operation === 'x') return multiply(num1,num2);
+    else if (operation === '/') return divide(num1,num2);
 }
+
